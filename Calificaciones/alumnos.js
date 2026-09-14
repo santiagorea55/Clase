b12dac1cf1dfe6535d4bc39e27a179afb18b864b
santiagorea55 <santiagorea0369@gmail.com>
2026-09-13 18:35:00 -0600

    Update alumnos.js

--- Calificaciones/alumnos.js
+++ Calificaciones/alumnos.js
@@ -21,7 +21,7 @@ formulario.addEventListener("submit", function (evento) {
     let cal1 = Number(materia1.value);
     let cal2 = Number(materia2.value);
     let cal3 = Number(materia3.value);
-    let promedio: ((cal1 + cal2 + cal3) / 3);
+    let promedio = ((cal1 + cal2 + cal3) / 3);
     let estatus = "";
         if (promedio >= 7) {
             estatus = "Aprobado";
